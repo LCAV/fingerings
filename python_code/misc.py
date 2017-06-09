383cda86fdb3f3ff54b9e51cd31740223552a6e3
Raphaël Latty <raphael.latty@epfl.ch>
2017-06-09 21:07:26 +0200

    submitted project version

--- python_code/misc.py
+++ python_code/misc.py
@@ -28,27 +28,27 @@ fundamental_to_fingerings = {
 
 
 
-def estimate_inharmonicity_poly(num_recordings, LHS, num_frames, max_mode):
+def estimate_inharmonicity_poly(num_recordings, stacked_freq_ratios, num_frames, max_mode):
     
     """ Compute the inharmonicity coefficient from the estimated partials in successive time
-        frames, using the polynomial fit (method 2).
+        frames by fitting a degree-4 polynomial to the stacked, squared frequency ratios.
         
         input:
-            - num_recordings: the number of recordings
-            - LHS: the left-hand side vector of the system
+            - num_recordings: the number of recordings used to estimate the inharmonicity
+            - stacked_freq_ratios: stacked, squared frequency ratios (left-hand side vector of the system) 
             - num_frames: the number of frames per recording
             - max_mode: the maximum partial number we consider
         
         output:
-            - b: the estimated inharmonicity constant
+            - b_hat: the estimated inharmonicity constant
     """
     
     # initialize the system
-    LHS = np.hstack(LHS)
+    LHS = np.hstack(stacked_freq_ratios)
     system_matrix = np.matlib.repmat(np.vstack((np.arange(1, max_mode + 1)**2, \
-                                            np.arange(1, max_mode + 1)**4)).T, num_recordings * num_frames, 1)
+                                            np.arange(1, max_mode + 1)**4)).T, num_recordings*num_frames, 1)
     
-    # 2D Least squares fit (polynomial fit)
+    # 2D least squares fit (polynomial fit)
     coef, _, _, _ = np.linalg.lstsq(system_matrix, LHS)
     b_hat = coef[1]
     
@@ -58,16 +58,17 @@ def estimate_inharmonicity_poly(num_recordings, LHS, num_frames, max_mode):
 
 def find_best_fingering(gammas, estimated_fundamental, b_hat, string_full_length):
     
-    """ Finds the best fingering corresponding to the estimated fundamental and inharmonicity.
+    """ Finds the best fingering for the estimated fundamental and inharmonicity constant.
         
         input:
             - gammas: the output of our training phase (inharmonicity profiles of each string)
-            - estimated_fundamental: the estimated fundamental of the test recording
-            - b_hat: the estimated inharmonicity coefficient of the test recording
+            - estimated_fundamental: the estimated fundamental frequency of the test recording
+            - b_hat: the estimated inharmonicity constant of the test recording
             - string_full_length: the full length of the strings
         
         output:
-            - b_candidates: the inharmonicity "candidates
+            - fingerings_candidates: the possible fingerings for the estimated fundamental
+            - b_candidates: the inharmonicity of the potential fingering candidates
             - best_fingering: a pair (string_number, fretboard_position) corresponding to 
                 the best fingering (closest inharmonicity)
     """
@@ -77,27 +78,28 @@ def find_best_fingering(gammas, estimated_fundamental, b_hat, string_full_length
     gamma_candidates = []
     lengths_candidates = []
     
-    # search in our dictionary (fundamental-to-fingering mapping)
+    # search in our dictionary of fundamental-to-fingering mappings
     if estimated_fundamental in fundamental_to_fingerings.keys():
         fingerings_candidates = fundamental_to_fingerings[estimated_fundamental]
     else:
         print('Error: Fundamental not found!')
+        return
     
-    print("\n\tPotential fingerings candidates: {fing_cand}".format(fing_cand=fingerings_candidates))
+    print("\n\t\tPotential fingerings: {fing_cand}".format(fing_cand=fingerings_candidates))
     
-    # For all potential fingerings
+    # for all potential fingerings
     for fingering in fingerings_candidates:
         string_num, fret_pos = fingering
         gamma_candidates.append(gammas[string_num - 1])
         lengths_candidates.append(compute_string_length(string_full_length, fret_pos))
     
-    b_candidates = np.array(gamma_candidates) / np.array(lengths_candidates)**2
+    # compute the inharmonicity constants corresponding to all the possible fingerings
+    b_candidates = np.array(gamma_candidates)/np.array(lengths_candidates)**2
+    print("\t\tCorresponding inharmonicity constants: {bs}\n".format(bs=b_candidates))
     
-    print("\tCorresponding inharmonicity coefficients: {bs}".format(bs=b_candidates))
-    
-    # find the best fingering (closest inharmonicity coefficient)
+    # find the "best" fingering (closest inharmonicity constant)
     best_fingering_idx = np.argmin(np.abs(b_candidates - b_hat))
     best_fingering = fingerings_candidates[best_fingering_idx]
     
-    return b_candidates, best_fingering
+    return fingerings_candidates, b_candidates, best_fingering
 
